e3cf0b7fdd7057ebdd20d96e683f892a413189c7
Darong Mean <darong012@gmail.com>
2014-03-07 23:22:01 +0700

    create a new project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to semantic-om
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,28 @@
+(defproject semantic-om "0.0.1-SNAPSHOT"
+  :description "Semantic-UI integrated with Clojurescript OM framework"
+  :url "http://example.com/FIXME"
+  :license {:name "The MIT License"
+            :url "LICENSE"
+            :distribution :repo}
+
+  :min-lein-version "2.3.4"
+
+  :source-paths ["src/clj" "src/cljs"]
+
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2173"]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+                 [om "0.5.1"]
+                 [com.facebook/react "0.9.0"]]
+  
+  :plugins [[lein-cljsbuild "1.0.2"]]
+
+  :hooks [leiningen.cljsbuild]
+
+  :cljsbuild
+  {:builds {:semantic-om
+            {:source-paths ["src/cljs"]
+             :compiler
+             {:output-to "dev-resources/public/js/semantic_om.js"
+              :optimizations :advanced
+              :pretty-print false}}}})
