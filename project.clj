857da5ef0f1a7ce432efd588447670c763f48a1c
Darong Mean <darong012@gmail.com>
2014-03-08 11:16:24 +0700

    fix browser-repl and update reactjs to 0.9.0

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
   :source-paths ["src/clj" "src/cljs"]
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2173"]
+                 [org.clojure/clojurescript "0.0-2156"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [om "0.5.1"]
                  [com.facebook/react "0.9.0"]]
